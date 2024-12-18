360a959df7e06a69ebe36c5dca03f5b227ded423
v.s.shashkov <isip_v.s.shashkov@mpt.ru>
2024-12-18 11:52:36 +0300

    Projectformylovelycollege

--- Cursach1/models.py
+++ Cursach1/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class User(models.Model):
     username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=255)
+    
     class Meta:
         app_label = 'PractPractica'
         db_table = 'Users'
